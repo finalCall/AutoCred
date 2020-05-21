0c2f04edb468d6d49d999cc33f94d4f948bfe000
iamNamanKhare <namankhare42@gmail.com>
2020-05-21 22:12:01 +0530

    Add Basic Payment Slider

--- app/src/main/java/com/dhruv/app/paySpinner.java
+++ app/src/main/java/com/dhruv/app/paySpinner.java
@@ -3,6 +3,9 @@ package com.dhruv.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import co.aenterhy.toggleswitch.ToggleSwitchButton;
 
 public class paySpinner extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class paySpinner extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_spinner);
+
+        ToggleSwitchButton toggle = (ToggleSwitchButton) findViewById(R.id.toggle);
+        toggle.setOnTriggerListener(new ToggleSwitchButton.OnTriggerListener() {
+            @Override
+            public void toggledUp() {
+                Toast.makeText(getApplicationContext(),"Pay", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void toggledDown() {
+                Toast.makeText(getApplicationContext(),"Recieve", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
